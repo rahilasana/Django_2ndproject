50f617279beaf67b1465f6887b714e81a38a17a3
rahila sana <sanakhan@email.com>
2026-09-04 11:48:12 +0500

    add some features

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,7 +5,7 @@ from main import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home,name="home"),
-    path('/about/', views.about,name="about"),
-    path('/services/', views.services,name="services"),
-    path('/contact/', views.contact,name="contact"),
+    path('about/', views.about,name="about"),
+    path('services/', views.services,name="services"),
+    path('contact/', views.contact,name="contact"),
 ]
